0b0031863486d5ab5099ec088a6a6ff69f8c61fc
Jamie <77027304+itisjamied@users.noreply.github.com>
2024-05-15 12:03:59 -0400

    make qr codes generate to svgs

--- qr.py
+++ qr.py
@@ -1,23 +1,25 @@
-# Importing library
 import qrcode
 import os
+from qrcode.image.svg import SvgImage
 
 # Ensure the folder exists
-folder = 'qrcodes'
+folder = 'svg_qrcodes'
 os.makedirs(folder, exist_ok=True)
- 
+
 # Data to be encoded
+menuLink = "https://www.ctechinteractives.pro/"
 presentLink = 'https://dorchester-micro-games.vercel.app/present/present.html'
 pastLink = 'https://dorchester-micro-games.vercel.app/past/past.html'
 futureLink = 'https://dorchester-micro-games.vercel.app/future/future.html'
- 
-# Encoding data using make() function
-presentImg = qrcode.make(presentLink)
-pastImg = qrcode.make(pastLink)
-futureImg = qrcode.make(futureLink)
 
+# Create a QRCode object with SVG factory
+menuImg = qrcode.make(menuLink, image_factory=SvgImage)
+presentImg = qrcode.make(presentLink, image_factory=SvgImage)
+pastImg = qrcode.make(pastLink, image_factory=SvgImage)
+futureImg = qrcode.make(futureLink, image_factory=SvgImage)
 
-# Saving as an image file in the specified folder
-presentImg.save(os.path.join(folder, 'present.png'))
-pastImg.save(os.path.join(folder, 'past.png'))
-futureImg.save(os.path.join(folder, 'future.png'))
+# Saving as SVG files in the specified folder
+menuImg.save(os.path.join(folder, 'menu.svg'))
+presentImg.save(os.path.join(folder, 'present.svg'))
+pastImg.save(os.path.join(folder, 'past.svg'))
+futureImg.save(os.path.join(folder, 'future.svg'))
